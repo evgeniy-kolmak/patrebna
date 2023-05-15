47fcc42c9a3468cc8e3a7961d60f3a23fd7ee544
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-05-15 20:18:06 +0300

    addded cron

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import schedule from 'node-schedule';
 import axios from 'axios';
 import { bot } from './helpers/telegram/bot';
 import { compareCollections, pause, getOldIdAds } from './helpers/utils';
@@ -17,40 +18,43 @@ void (async () => {
   await commandStart(users, usersIds);
   await commandChangeurl();
 
-  for (const id of usersIds) {
-    const url = await db.getUserUrl(id);
-    let html = '';
+  schedule.scheduleJob('*/5 * * * *', async () => {
+    console.log(new Date().toLocaleTimeString('ru-RU'));
+    for (const id of usersIds) {
+      const url = await db.getUserUrl(id);
+      let html = '';
 
-    try {
-      const { data } = await axios.get(url);
-      html = data;
-    } catch (error) {
-      console.log(error);
-    }
-
-    const typeUrlParser = await db.getUserTypeParser(id);
-    const parserData = parserAds(typeUrlParser, html);
-
-    const saveAds = await db.getSavedAds(id);
-    const newIds = compareCollections(saveAds, parserData);
-
-    const statusCollectionAds = await db.isAdsEmpty(id);
-    for (const newId of newIds) {
-      const data = parserData[newId];
-      await db.setNewAd(data, id);
-      await pause(2500);
-      if (statusCollectionAds) {
-        bot.sendPhoto(id, `${data.img_url}`, {
-          caption: `Появился новый товар <b>${data.title}</b>, c ценой <b>${data.price}</b>\nвот ссылка: ${data.url}`,
-          parse_mode: 'HTML',
-        });
+      try {
+        const { data } = await axios.get(url);
+        html = data;
+      } catch (error) {
+        console.log(error);
       }
+
+      const typeUrlParser = await db.getUserTypeParser(id);
+      const parserData = parserAds(typeUrlParser, html);
+
+      const saveAds = await db.getSavedAds(id);
+      const newIds = compareCollections(saveAds, parserData);
+
+      const statusCollectionAds = await db.isAdsEmpty(id);
+      for (const newId of newIds) {
+        const data = parserData[newId];
+        await db.setNewAd(data, id);
+        await pause(2500);
+        if (statusCollectionAds) {
+          bot.sendPhoto(id, `${data.img_url}`, {
+            caption: `Появился новый товар <b>${data.title}</b>, c ценой <b>${data.price}</b>\nвот ссылка: ${data.url}`,
+            parse_mode: 'HTML',
+          });
+        }
+      }
+
+      console.log(`Добавлено новых объявлений ${newIds.length} в базу`);
     }
+  });
 
-    console.log(`Добавлено новых объявлений ${newIds.length} в базу`);
-  }
-
-  //  const collectionOldId =  getOldIdAds(saveAds);
+  //  const collectionOldId = getOldIdAds(saveAds);
 
   // if (collectionOldId.length) {
   //   for (let i = 0; i > newIds.length; i++) {
