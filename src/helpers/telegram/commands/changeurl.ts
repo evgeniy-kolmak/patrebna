83a06ccdbcd7ab6f10d5e5965c70a840269949b7
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-06-11 11:25:44 +0300

    fixed behavior if there is no registration

--- src/helpers/telegram/commands/changeurl.ts
+++ src/helpers/telegram/commands/changeurl.ts
@@ -1,49 +1,64 @@
 import { bot } from '../bot';
 import axios from 'axios';
 import db from '../../database';
-import { IUser } from '../../database';
+import { IUser, ICollection } from '../../database';
 import { typeUrlParser } from '../../typeUrlParser';
 
-export async function commandChangeurl(): Promise<void> {
-  bot.onText(/\/changeurl/, (ctx1) => {
+export async function commandChangeurl(
+  users: ICollection<IUser>,
+  usersIds: string[],
+): Promise<void> {
+  bot.onText(/\/changeurl/, async (ctx1) => {
+    users = await db.getUsers();
+    usersIds = users ? Object.keys(users) : [];
+
     const { id } = ctx1.chat;
-
-    bot.sendMessage(id, '💬 Хотите изменить ссылку?', {
-      reply_markup: {
-        inline_keyboard: [[{ text: ' 👌 Да, хочу!', callback_data: 'reply' }]],
-      },
-    });
-
-    bot.on('callback_query', async (query) => {
-      const promptLink = await bot.sendMessage(
-        id,
-        '⚙️ Укажите ссылку с параметрами для отслеживания типа - https://kufar.by/l/город/товар/',
-        {
-          reply_markup: {
-            force_reply: true,
-          },
+    if (usersIds.includes(id.toString())) {
+      bot.sendMessage(id, '💬 Хотите изменить ссылку?', {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: ' 👌 Да, хочу!', callback_data: 'reply' }],
+          ],
         },
-      );
-      const { message_id } = promptLink;
-      bot.onReplyToMessage(id, message_id, async (message) => {
-        const { entities, text, from } = message;
-        if (entities?.[0].type === 'url') {
-          try {
-            if (text) {
-              await db.removeAds(id.toString());
-              await axios.get(text);
-              await typeUrlParser(text, from as IUser);
-              await db.setUrlUser(text, from as IUser);
-              bot.sendMessage(
-                id,
-                '🎯 Все прошло успешно, ожидайте обновлений!',
-              );
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        }
       });
-    });
+
+      bot.on('callback_query', async (query) => {
+        const promptLink = await bot.sendMessage(
+          id,
+          '⚙️ Укажите ссылку с параметрами для отслеживания типа - https://kufar.by/l/город/товар/',
+          {
+            reply_markup: {
+              force_reply: true,
+            },
+          },
+        );
+        const { message_id } = promptLink;
+        bot.onReplyToMessage(id, message_id, async (message) => {
+          const { entities, text, from } = message;
+          if (entities?.[0].type === 'url') {
+            try {
+              if (text) {
+                await db.removeAds(id.toString());
+                await axios.get(text);
+                await typeUrlParser(text, from as IUser);
+                await db.setUrlUser(text, from as IUser);
+                bot.sendMessage(
+                  id,
+                  '🎯 Все прошло успешно, ожидайте обновлений!',
+                );
+              }
+            } catch (error) {
+              console.log(error);
+            }
+          }
+        });
+      });
+    } else {
+      bot.sendMessage(
+        id,
+        '😟 Вы еще не зарегистрированы! Что бы начать взаимодействовать с ботом выберите команду <a>/start</a>',
+        { parse_mode: 'HTML' },
+      );
+    }
   });
 }
