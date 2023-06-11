63d6c2f59fedccf4275a760e3b1cc808acfdd722
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-06-11 13:05:01 +0300

    Add button 'back' & helper for error

--- src/helpers/telegram/commands/changeurl.ts
+++ src/helpers/telegram/commands/changeurl.ts
@@ -3,55 +3,69 @@ import axios from 'axios';
 import db from '../../database';
 import { IUser, ICollection } from '../../database';
 import { typeUrlParser } from '../../typeUrlParser';
+import { errorMsg } from '../../errorMessage';
 
 export async function commandChangeurl(
   users: ICollection<IUser>,
   usersIds: string[],
 ): Promise<void> {
-  bot.onText(/\/changeurl/, async (ctx1) => {
+  bot.onText(/\/changeurl/, async (ctx) => {
     users = await db.getUsers();
     usersIds = users ? Object.keys(users) : [];
 
-    const { id } = ctx1.chat;
+    const { id } = ctx.chat;
     if (usersIds.includes(id.toString())) {
       bot.sendMessage(id, '💬 Хотите изменить ссылку?', {
         reply_markup: {
           inline_keyboard: [
-            [{ text: ' 👌 Да, хочу!', callback_data: 'reply' }],
+            [{ text: ' 👌 Да, хочу!', callback_data: 'change' }],
+            [{ text: ' ◀ Назад', callback_data: 'back' }],
           ],
         },
       });
 
       bot.on('callback_query', async (query) => {
-        const promptLink = await bot.sendMessage(
-          id,
-          '⚙️ Укажите ссылку с параметрами для отслеживания типа - https://kufar.by/l/город/товар/',
-          {
-            reply_markup: {
-              force_reply: true,
-            },
-          },
-        );
-        const { message_id } = promptLink;
-        bot.onReplyToMessage(id, message_id, async (message) => {
-          const { entities, text, from } = message;
-          if (entities?.[0].type === 'url') {
-            try {
-              if (text) {
-                await db.removeAds(id.toString());
-                await axios.get(text);
-                await typeUrlParser(text, from as IUser);
-                await db.setUrlUser(text, from as IUser);
-                bot.sendMessage(
-                  id,
-                  '🎯 Все прошло успешно, ожидайте обновлений!',
-                );
+        const msgKeyboard = ctx.message_id + 1;
+        switch (query.data) {
+          case 'change': {
+            bot.deleteMessage(id, msgKeyboard);
+            const promptLink = await bot.sendMessage(
+              id,
+              '⚙️ Укажите ссылку с параметрами для отслеживания типа - https://kufar.by/l/город/товар/',
+              {
+                reply_markup: {
+                  force_reply: true,
+                },
+              },
+            );
+
+            const { message_id } = promptLink;
+            bot.onReplyToMessage(id, message_id, async (message) => {
+              const { entities, text, from } = message;
+              if (entities?.[0].type === 'url') {
+                try {
+                  if (text) {
+                    await db.removeAds(id.toString());
+                    await axios.get(text);
+                    await typeUrlParser(text, from as IUser);
+                    await db.setUrlUser(text, from as IUser);
+                    bot.sendMessage(
+                      id,
+                      '🎯 Все прошло успешно, ожидайте обновлений!',
+                    );
+                  }
+                } catch (error) {
+                  errorMsg(id, '/changeurl');
+                }
               }
-            } catch (error) {
-              console.log(error);
-            }
+            });
+            break;
           }
-        });
+          case 'back': {
+            bot.deleteMessage(id, msgKeyboard);
+            bot.sendMessage(id, 'text');
+          }
+        }
       });
     } else {
       bot.sendMessage(
