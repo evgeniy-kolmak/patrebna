fd80ea3fb3314b2dc91dbf38262a5bd90dafaaab
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-09-02 15:14:24 +0300

    Added ts-node and connect with webhook

--- src/helpers/telegram/bot.ts
+++ src/helpers/telegram/bot.ts
@@ -5,13 +5,21 @@ import TelegramBot from 'node-telegram-bot-api';
 const TOKEN = conf.tokenBot;
 const URL = conf.webhook.url;
 const PORT = conf.webhook.port;
-export const bot = new TelegramBot(TOKEN, {
+
+const options = {
   webHook: {
     port: PORT,
+    key: 'key.pem',
+    cert: 'crt.pem',
   },
+};
+
+export const bot = new TelegramBot(TOKEN, options);
+
+bot.setWebHook(`${URL}:${PORT}/bot${TOKEN}`, {
+  certificate: options.webHook.cert,
 });
 
-bot.setWebHook(`${URL}/${TOKEN}`);
 process.env['NTBA_FIX_350'] = '1';
 
 cbquery();
