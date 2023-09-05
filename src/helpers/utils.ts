947d3307fb179c8668e861ea2de8b3c87854ac06
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-09-05 18:38:45 +0300

    Added compareLengthPathPackages & truncateString

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,4 +1,5 @@
-import { ICollection, IAd } from './database';
+import { ICollection, IAd, ITrack } from './database';
+import { IDataApiError, getDataForTrackNumber } from './tracker/addTracks';
 
 export function pause(val = 500) {
   return new Promise((resolve) => {
@@ -13,3 +14,24 @@ export function compareCollections(
   return Object.keys(updates).filter((key: string) => !src[key]);
 }
 
+export async function compareLengthPathPackages(
+  dataTrack: ITrack | IDataApiError,
+) {
+  if ('trackNumber' in dataTrack) {
+    const newDataTrack = await getDataForTrackNumber(dataTrack.trackNumber);
+    if ('trackNumber' in newDataTrack) {
+      if (newDataTrack.lengthPath !== dataTrack.lengthPath) {
+        const dataTrackMessage: ITrack = {
+          infoPoint: newDataTrack.infoPoint,
+          trackNumber: newDataTrack.trackNumber,
+          lengthPath: newDataTrack.lengthPath,
+          comment: newDataTrack.comment,
+        };
+        return dataTrackMessage;
+      }
+    }
+  }
+}
+
+export const truncateString = (s: string) =>
+  s.length > 130 ? `${s.substring(0, 130)} ...` : s;
