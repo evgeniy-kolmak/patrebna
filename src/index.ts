920f6b39a788bc34a99d2564df8d2f5d98486c4a
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-09-10 13:24:24 +0300

    Added track packages

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import schedule from 'node-schedule';
-import axios from 'axios';
-import { bot } from './helpers/telegram/bot';
-import { compareCollections, pause } from './helpers/utils';
+import { pause } from './helpers/utils';
 import db from './helpers/database';
-import { parserAds } from './helpers/parser/parserAds';
 import { commandStart } from './helpers/telegram/commands/start';
 import { commandHelp } from './helpers/telegram/commands/help';
 import { commandChangeurl } from './helpers/telegram/commands/change_url';
+import { commandTrackPackages } from './helpers/telegram/commands/track_packages';
 import { commandStop } from './helpers/telegram/commands/stop';
+
+import parseKufar from './helpers/tasks/parseKufar';
+import trackEvropochta from './helpers/tasks/trackEvropochta';
 
 void (async () => {
   await pause(1000);
@@ -17,47 +18,25 @@ void (async () => {
 
   await commandStart(users, usersIds);
   await commandChangeurl(users, usersIds);
+  await commandTrackPackages();
   await commandStop(users, usersIds);
   commandHelp();
 
   schedule.scheduleJob('*/15 * * * *', async () => {
-    console.log(new Date().toLocaleTimeString('ru-RU'));
-    for (const id of usersIds) {
-      const url = await db.getUserUrl(id);
-      let html = '';
-      if (url) {
-        try {
-          const { data } = await axios.get(url);
-          html = data;
-        } catch (error) {
-          console.error(error);
-        }
+    try {
+      await parseKufar(usersIds);
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
-        const typeUrlParser = await db.getUserTypeParser(id);
-        const parserData = parserAds(typeUrlParser, html);
+  await parseKufar(usersIds);
 
-        const saveAds = await db.getSavedAds(id);
-        const newIds = compareCollections(saveAds, parserData);
-
-        const statusCollectionAds = await db.isAdsEmpty(id);
-        for (const newId of newIds) {
-          const data = parserData[newId];
-          await db.setNewAd(data, id);
-          await pause(2500);
-          if (statusCollectionAds) {
-            bot.sendPhoto(id, `${data.img_url}`, {
-              caption: `Появилось новое объявление: <b>${
-                data.title
-              }</b>, c ценой <b>${data.price}</b>.\n<i>${
-                data?.description ?? 'Описание отсутствует к данному объявлению'
-              }</i>\nВот ссылка: ${data.url}`,
-              parse_mode: 'HTML',
-            });
-          }
-        }
-
-        console.log(`Добавлено новых объявлений ${newIds.length} в базу`);
-      }
+  schedule.scheduleJob('0 */1 * * *', async () => {
+    try {
+      await trackEvropochta(usersIds);
+    } catch (error) {
+      console.error(error);
     }
   });
 })();
