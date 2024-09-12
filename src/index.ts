c8d377161345efded0b4de28ada742fc1c39735b
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2024-09-12 16:16:11 +0300

    chore: clear index file

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,13 @@
-import schedule from 'node-schedule';
-import { pause } from './helpers/utils';
-import db from './helpers/database';
-import { commandStart } from './helpers/telegram/commands/start';
-import { commandHelp } from './helpers/telegram/commands/help';
-import { commandChangeurl } from './helpers/telegram/commands/change_url';
-import { commandTrackPackages } from './helpers/telegram/commands/track_packages';
-import { commandStop } from './helpers/telegram/commands/stop';
-
-import parseKufar from './helpers/tasks/parseKufar';
-import trackEvropochta from './helpers/tasks/trackEvropochta';
+import db from 'config/db/databaseServise';
+import parseKufar from 'parsers/kufar/tasks/parseKufar';
+import { scheduleJob } from 'node-schedule';
 
 void (async () => {
-  await pause(1000);
+  const users: number[] = await db.getUserForParse();
 
-  let users = await db.getUsers();
-  let usersIds = users ? Object.keys(users) : [];
-
-  await commandStart(users, usersIds);
-  await commandChangeurl(users, usersIds);
-  await commandTrackPackages(users, usersIds);
-  await commandStop(users, usersIds);
-  commandHelp();
-
-  schedule.scheduleJob('*/15 * * * *', async () => {
+  scheduleJob('*/15 * * * *', async () => {
     try {
-      await parseKufar(usersIds);
-    } catch (error) {
-      console.error(error);
-    }
-  });
-
-  schedule.scheduleJob('0 */1 * * *', async () => {
-    try {
-      await trackEvropochta(usersIds);
+      await parseKufar(users);
     } catch (error) {
       console.error(error);
     }
