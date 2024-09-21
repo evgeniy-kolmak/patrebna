0f7428a830032c1720763b3ac860844211e41353
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2024-09-21 12:29:26 +0300

    feat(webhook): add connet to webhook

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -3,7 +3,27 @@ import 'config/i18n/i18n';
 import cbquery from 'bot/cbquery';
 
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN ?? '';
+const PORT = Number(process.env.WEBHOOK_PORT) ?? 8443;
+const URL = process.env.WEBHOOK_URL ?? '';
 
-export const bot = new TelegramBot(TOKEN, { polling: true });
+const options = {
+  webHook: {
+    port: PORT,
+    cert: 'crt.pem',
+  },
+};
+
+export const bot = new TelegramBot(TOKEN, options);
+
+void (async () => {
+  try {
+    await bot.setWebHook(`${URL}:${PORT}/bot${TOKEN}`, {
+      certificate: options.webHook.cert,
+    });
+    console.info('Webhook set is successfully');
+  } catch (error) {
+    console.error('The certificate could not be installed', error);
+  }
+})();
 
 cbquery();
