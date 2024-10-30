2849dafd6ea15969d1a905b54cf9043be13bd8b5
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2024-10-30 18:32:39 +0300

    fix(users): fix deleting blocked users

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,35 @@ import { type Error } from 'config/types';
 
 void (async () => {
   scheduleJob('*/15 * * * *', async () => {
-    try {
-      await parseKufar((await db.getUserForParse()) as number[]);
-    } catch (error) {
-      const err = error as Error;
-      if (
-        err?.response?.body?.description ===
-          'Forbidden: bot was blocked by the user' &&
-        err?.response?.body?.error_code === 403
-      ) {
-        const url = err?.response?.request?.href;
-        const match = url.match(/chat_id=(\d+)/);
-        const chatId = match ? Number(match[1]) : null;
-        if (chatId) {
-          await db.removeUser(chatId);
-        } else {
-          console.log('Заблокированный пользователь не найден!');
-        }
-      } else {
-        console.error(error);
-      }
-    }
+    await taskKufar((await db.getUserForParse()) as number[]);
   });
 })();
+
+async function taskKufar(users: number[]): Promise<void> {
+  try {
+    await parseKufar(users);
+  } catch (error) {
+    const err = error as Error;
+    if (
+      err?.response?.body?.description ===
+        'Forbidden: bot was blocked by the user' &&
+      err?.response?.body?.error_code === 403
+    ) {
+      const url = err?.response?.request?.href;
+      const match = url.match(/chat_id=(\d+)/);
+      const chatId = match ? Number(match[1]) : null;
+      if (chatId) {
+        await db.removeUser(chatId);
+        const indexOfUser = users.findIndex((user) => indexOfUser === user);
+        if (indexOfUser !== users.length - 1) {
+          await taskKufar(users.slice(indexOfUser + 1));
+        }
+        console.log('Заблокированный пользователь был удален!');
+      } else {
+        console.log('Заблокированный пользователь не найден!');
+      }
+    } else {
+      console.error(error);
+    }
+  }
+}
