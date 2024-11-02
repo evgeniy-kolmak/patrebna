e1bbe93623f07b74fa9c9e965266ff82502fb3d5
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2024-11-02 20:27:49 +0300

    chore: fix mistakes

--- src/bot/commands/profile.ts
+++ src/bot/commands/profile.ts
@@ -13,7 +13,7 @@ export default (): void => {
       const isUser = await db.getUser(userID);
       if (isUser) {
         const profile = await db.getProfile(userID);
-        const dataProfile = `<b>${t('ФИО')}</b>: ${profile?.last_name ?? ''} ${profile?.first_name ?? ''}${profile?.username ? `\nç<b>${t('Псевдоним')}</b>: ${profile?.username ?? ''}` : ''}\n<b>${t('Подписка')}</b>: ${profile?.premium ? new Date(profile.premium * 1000).toLocaleDateString('ru-RU') : '➖'} \n<b>${t('Ссылка')}</b>: ${profile?.link ?? '❌'}\n<b>${t('Количество объявлений')}</b>: ${profile?.count_ads ?? 0}`;
+        const dataProfile = `<b>${t('ФИО')}</b>: ${profile?.last_name ?? ''} ${profile?.first_name ?? ''}${profile?.username ? `\n<b>${t('Псевдоним')}</b>: ${profile?.username ?? ''}` : ''}\n<b>${t('Подписка')}</b>: ${profile?.premium ? new Date(profile.premium * 1000).toLocaleDateString('ru-RU') : '➖'} \n<b>${t('Ссылка')}</b>: ${profile?.link ?? '❌'}\n<b>${t('Количество объявлений')}</b>: ${profile?.count_ads ?? 0}`;
         await bot.sendMessage(userID, dataProfile, {
           parse_mode: 'HTML',
           disable_web_page_preview: true,
