9f50022e22664efb82528db7d916a874d8e8c446
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2024-12-22 18:47:32 +0300

    feat(redis): the Redis has been added in the project

---
+++ src/config/redis/redisService.ts
@@ -0,0 +1,38 @@
+import Redis from 'ioredis';
+
+class RedisService {
+  private readonly redis: Redis;
+  constructor() {
+    const password = process.env.REDIS_PASSWORD ?? '';
+    this.redis = new Redis({
+      host: 'redis',
+      port: 6379,
+      tls: {
+        ca: './certs/ca.pem',
+        cert: './certs/client-cert.pem',
+        key: './certs/client-key.pem',
+      },
+      password,
+    });
+
+    this.redis.on(
+      'error',
+      console.error.bind(console, 'Error connecting to Redis:'),
+    );
+
+    this.redis.once('open', () => {
+      console.log('Connected to Redis successfully!');
+    });
+  }
+
+  async setCache(key: string, value: any, ttl: number): Promise<void> {
+    await this.redis.set(key, JSON.stringify(value), 'EX', ttl);
+  }
+
+  async getCache(key: string): Promise<string | null> {
+    return await this.redis.get(key);
+  }
+}
+
+const redis = new RedisService();
+export default redis;
