3eaf14a7a51aa92b8471fd6e9c2a950f44361218
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2025-02-06 20:39:58 +0300

    fix(model): fix model for ads

--- src/config/db/models/Parser.ts
+++ src/config/db/models/Parser.ts
@@ -5,7 +5,20 @@ import { DataParser } from 'config/db/models/DataParser';
 const ParserSchema = new Schema(
   {
     kufar: {
-      kufarAds: [{ type: Schema.Types.ObjectId, ref: KufarAd }],
+      kufarAds: {
+        type: [
+          {
+            urlId: { type: Number, required: true },
+            ads: [{ type: Schema.Types.ObjectId, ref: KufarAd }],
+          },
+        ],
+        validate: [
+          {
+            validator: (value: unknown[]) => value.length <= 3,
+            message: 'Поле "kufarAds" не может содержать больше 3 элементов.',
+          },
+        ],
+      },
       dataParser: { type: Schema.Types.ObjectId, ref: DataParser },
     },
   },
