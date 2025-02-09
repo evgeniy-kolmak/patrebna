3a5a1ec8df84013c48cdd7bf943135d6b10a10d1
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2025-02-09 16:17:24 +0300

    feat(command): added command 'help'

---
+++ src/bot/commands/help.ts
@@ -0,0 +1,16 @@
+import { bot } from 'bot';
+import i18next, { t } from 'i18next';
+import { getUserLanguage } from 'config/lib/helpers/cacheLaguage';
+
+export default (): void => {
+  const regex = /Помощь|Дапамога/;
+  bot.onText(regex, (ctx) => {
+    void (async () => {
+      const userID = ctx.chat.id;
+      await i18next.changeLanguage(getUserLanguage(userID));
+      await bot.sendMessage(userID, t('Техподдержка'), {
+        parse_mode: 'HTML',
+      });
+    })();
+  });
+};
