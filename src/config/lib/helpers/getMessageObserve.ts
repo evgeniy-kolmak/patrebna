16812018504144d8d19fa8e78b674b4862aa5d5c
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2025-02-09 17:54:24 +0300

    feat(helper): added an assistant to display information about the link

---
+++ src/config/lib/helpers/getMessageObserve.ts
@@ -0,0 +1,18 @@
+import db from 'config/db/databaseServise';
+import { t } from 'i18next';
+
+export async function getMessageObserve(
+  chatId: number,
+  urlId: number,
+): Promise<string> {
+  const urlInformation = await db.getUrlInformation(chatId, urlId);
+
+  return [
+    `🔍 <b>${t('Информация о ссылке')}</b>`,
+    '',
+    `${t('Вот данные по запрашиваемой ссылке')}:`,
+    `🔗 <b>${t('Ссылка')}:</b> ${urlInformation.url} `,
+    `📌 <b>${t('Состояние')}:</b> ${urlInformation.statusUrl ? t('Активное состояние') : t('Неактивное состояние')}`,
+    `🔢 <b>${t('Количество объявлений')}:</b> ${urlInformation.numberOfAds}`,
+  ].join('\n');
+}
