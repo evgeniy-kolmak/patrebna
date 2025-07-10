f03e55b29e399789e47ce6f040a798ba2a0470ea
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2025-07-10 18:19:04 +0300

    chore: added connection to db

--- src/api/server.ts
+++ src/api/server.ts
@@ -4,6 +4,7 @@ import { readFileSync } from 'fs';
 import webhookRoutes from 'api/routes/webhook.routes';
 import 'dotenv/config';
 import 'config/i18n/i18n';
+import db from 'config/db/databaseServise';
 
 const app = express();
 const PORT = 3000;
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use('/webhook', webhookRoutes);
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-https.createServer(options, app).listen(PORT, () => {
+https.createServer(options, app).listen(PORT, async () => {
+  await db.openConnection();
   console.log(`Cервер запущен на порту ${PORT}.`);
 });
