9ef605036d5ecadb24a65e8170b2a333ae64de85
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2025-11-22 15:21:35 +0300

    feat(api): added new routes, improved code

--- src/api/routes/webhook.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { bepaidWebhookHandler } from 'api/controllers/webhook.controller';
-
-const router = Router();
-
-router.post('/bepaid', bepaidWebhookHandler);
-
-export default router;
