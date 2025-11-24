ecaf50f3e65dec402b951c4aaa1010178c243cc9
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2025-11-24 18:34:23 +0300

    chore: fixed mistakes in text

--- src/config/lib/helpers/sendExpendedMessageOfNewAd.ts
+++ src/config/lib/helpers/sendExpendedMessageOfNewAd.ts
@@ -45,7 +45,7 @@ export async function sendExpendedMessageOfNewAd(
   } = parameters;
 
   const message = [
-    `${t('Появилось')} <a href="${url}">${t('Новое объявление')}</a>\u00A0: <b>${title}</b>, ${t('В локации')} <b>${region}</b>, ${t('C ценой')} <b>${price}</b>.`,
+    `${t('Появилось')} <a href="${url}">${t('Новое объявление')}</a>: <b>${title}</b>\u2060, ${t('В локации')} <b>${region}</b>, ${t('C ценой')} <b>${price}</b>.`,
     `<i>${truncateString(description, 500)}</i>`,
     size ? `<b>${t('Общая площадь')}</b>: ${size}м²` : '',
     square_meter ? `<b>${t('Цена')} за м²</b>: ${square_meter}$` : '',
